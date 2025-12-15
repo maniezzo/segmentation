4c01fe0937a728d49af3a7713a0e1f3765c81f83
maniezzo <vittorio.maniezzo@unibo.it>
2025-12-15 12:41:57 +0100

    init paper

--- foreNback/viewSol/main.py
+++ foreNback/viewSol/main.py
@@ -2,21 +2,24 @@ import numpy as np, pandas as pd
 import matplotlib.pyplot as plt
 
 def plotSol(dfsol,dfdata):
-   fig = plt.figure(figsize=(10,8))
-   plt.plot(dfdata.iloc[:,1],marker='o',linewidth=0,color='red')
+   fig = plt.figure(figsize=(10,6))
+   plt.plot(dfdata.iloc[:,1],marker='.',linewidth=0,color='red')
    for _, row in dfsol.iterrows():
       t1, t2, m, q = row['t1'], row['t2'], row['m'], row['q']
       x = [t1, t2]
       y = [m * t1 + q, m * t2 + q]
-      plt.plot(x, y, marker='o', label=f'm={m}, q={q}')
-   plt.legend()
-   plt.title(datafile)
+      plt.plot(x, y, linewidth=2, color="blue", label=f'm={m}')
+   #plt.legend()
+   plt.title(name)
+   plt.savefig(f"{name}.eps", format="eps")
    plt.show()
    return
 
 if __name__ == '__main__':
    solfile = "test_sol.csv"
    datafile = "M3//N2834.csv"
+   name = datafile.split("//", 1)[-1]
+   name = name.removesuffix(".csv")
    dfsol = pd.read_csv("..//F&Bsegmentation//"+solfile)
    dfdata = pd.read_csv("..//..//data//"+datafile)
    plotSol(dfsol,dfdata)
