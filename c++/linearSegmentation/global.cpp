9565071c763cfed0aa8a81bac1671c10fdcea1a6
maniezzo <vittorio.maniezzo@unibo.it>
2025-12-16 12:18:35 +0100

    boh

---
+++ c++/linearSegmentation/global.cpp
@@ -0,0 +1,7 @@
+#include "global.h"
+std::string baseDir = "";
+std::string dsName = "";
+std::vector<std::vector<int>> colids;
+std::vector<std::vector<int>> rowid;
+int maxIter = 0;
+int maxTime = 0;
---
+++ c++/linearSegmentation/global.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <vector>
+#include <string.h>
+#include <sstream>
+
+using namespace std;
+
+extern std::string baseDir;
+extern std::string dsName;    // dataset name (file col .csv)
+extern int maxIter;      // max nunm of lagrangian iterations
+extern int maxTime;      // max secs of lagrangian runs
+extern std::vector<std::vector<int>> rowids, colids;  // compression of indices, by row and by col
