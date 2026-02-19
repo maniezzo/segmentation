08ce868c0d99f7181d3a3e7b5384737d0cd8302b
maniezzo <vittorio.maniezzo@unibo.it>
2026-02-19 16:58:54 +0100

    tex

--- setar/main.py
+++ setar/main.py
@@ -8,39 +8,39 @@ from rpy2.robjects.packages import importr
 from rpy2.robjects import r
 
 if __name__ == "__main__":
-    name = "N1881"
-    df = pd.read_csv(f"c:\git\segmentation\data\M3\{name}.csv",usecols=[1])
-    y  = pd.Series(df.iloc[:,0])
-    
-    # R setup
-    tsDyn    = importr("tsDyn")
-    base     = importr("base")
-    graphics = importr("graphics")  # optional if you want R plots
-    
-    # Convert to R vector
-    r_y = ro.FloatVector(y.values)
-    
-    # Fit SETAR model
-    model = tsDyn.setar(r_y,
-                        m=2,        # AR order
-                        thDelay=1,  # threshold delay, how many instants ago I react to
-                        nthresh=1,  # number of thresholds, num  of regimes
-                        trim   = 0.3) # min percentage of data in each regime
-    
-    # Print R summary
-    print(base.summary(model))
-    
-    # Extract fitted values
-    fitted_r = np.array(model.rx2("fitted.values"))
-    
-    # Plot original data and fitted values
-    plt.figure(figsize=(12,5))
-    plt.plot(y, label="Original series", color="blue")
-    plt.plot(fitted_r, label="SETAR fitted values", color="red", linestyle="--")
-    plt.xlabel("Time")
-    plt.ylabel("Value")
-    plt.title(f"SETAR series {name}")
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-    print("fine")
+   name = "N1881"
+   df = pd.read_csv(f"c:\git\segmentation\data\M3\{name}.csv",usecols=[1])
+   y  = pd.Series(df.iloc[:,0])
+
+   # R setup
+   tsDyn    = importr("tsDyn")
+   base     = importr("base")
+   graphics = importr("graphics")  # optional if you want R plots
+
+   # Convert to R vector
+   r_y = ro.FloatVector(y.values)
+
+   # Fit SETAR model
+   model = tsDyn.setar(r_y,
+                       m=2,        # AR order
+                       thDelay=1,  # threshold delay, how many instants ago I react to
+                       nthresh=1,  # number of thresholds, num  of regimes
+                       trim   = 0.3) # min percentage of data in each regime
+
+   # Print R summary
+   print(base.summary(model))
+
+   # Extract fitted values
+   fitted_r = np.array(model.rx2("fitted.values"))
+
+   # Plot original data and fitted values
+   plt.figure(figsize=(12,5))
+   plt.plot(y, label="Original series", color="blue")
+   plt.plot(fitted_r, label="SETAR fitted values", color="red", linestyle="--")
+   plt.xlabel("Time")
+   plt.ylabel("Value")
+   plt.title(f"SETAR series {name}")
+   plt.legend()
+   plt.tight_layout()
+   plt.show()
+   print("fine")
